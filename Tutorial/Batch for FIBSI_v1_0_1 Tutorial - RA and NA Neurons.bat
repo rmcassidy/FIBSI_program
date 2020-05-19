fd26de3fba1d04412fe767528b58d7e97d60cc00
rmcassidy <rmcassidy@protonmail.ch>
2020-05-19 15:43:58 -0500

    Add files via upload

---
+++ Tutorial/Batch for FIBSI_v1_0_1 Tutorial - RA and NA Neurons.bat
@@ -0,0 +1,5 @@
+FIBSI_v1_0_1.py NeuronTutorial_RAandNA.csv -o NeuronTutorial_RAandNA_RatRAneuronresults -c 1 2 -r 0 A --norm rmd,20000 --evts dfy xc,200 yc,-1.5,1.5 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_1.py NeuronTutorial_RAandNA.csv -o NeuronTutorial_RAandNA_RatNAneuronresults -c 1 2 -r 0 B --norm rmd,20000 --evts dfy xc,200 yc,-1.5,1.5 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_1.py NeuronTutorial_RAandNA.csv -o NeuronTutorial_RAandNA_MouseNAneuronresults -c 1 2 -r 0 C --norm rmd,20000 --evts dfy xc,200 yc,-1.5,1.5 -p --plot raw fity dfy evts save_csv save_png,200
