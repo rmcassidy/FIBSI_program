184f5681edc3d347c230138300312e90295a5269
rmcassidy <max.neuro.odem@gmail.com>
2022-05-26 15:29:52 -0500

    For Revisions

---
+++ Zebrafish Gut Motility - June 2021/FIBSI Inputs/Revisions/10-22-2021/Batch for FIBSI_v1_0_3 - Zfish_Gut Motility Manuscript 10-22-21.bat
@@ -0,0 +1,88 @@
+#Unfed 7dpf (6 min)
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G006.csv -o AB_7dpf_unfed_new6min_G006_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_new6min_G006.csv -o AB_7dpf_unfed_new6min_G006_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G006.csv -o AB_7dpf_unfed_new6min_G006_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G007.csv -o AB_7dpf_unfed_new6min_G007_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_new6min_G007.csv -o AB_7dpf_unfed_new6min_G007_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G007.csv -o AB_7dpf_unfed_new6min_G007_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G008.csv -o AB_7dpf_unfed_new6min_G008_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_new6min_G008.csv -o AB_7dpf_unfed_new6min_G008_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G008.csv -o AB_7dpf_unfed_new6min_G008_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G009.csv -o AB_7dpf_unfed_new6min_G009_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_new6min_G009.csv -o AB_7dpf_unfed_new6min_G009_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G009.csv -o AB_7dpf_unfed_new6min_G009_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G011.csv -o AB_7dpf_unfed_new6min_G011_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_new6min_G011.csv -o AB_7dpf_unfed_new6min_G011_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G011.csv -o AB_7dpf_unfed_new6min_G011_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G012.csv -o AB_7dpf_unfed_new6min_G012_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_new6min_G012.csv -o AB_7dpf_unfed_new6min_G012_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G012.csv -o AB_7dpf_unfed_new6min_G012_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G014.csv -o AB_7dpf_unfed_new6min_G014_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_new6min_G014.csv -o AB_7dpf_unfed_new6min_G014_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_new6min_G014.csv -o AB_7dpf_unfed_new6min_G014_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+
+#Unfed 7dpf (20 min)
+FIBSI_v1_0_3.py AB_7dpf_unfed_20min_G008.csv -o AB_7dpf_unfed_20min_G008_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_20min_G008.csv -o AB_7dpf_unfed_20min_G008_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_20min_G008.csv -o AB_7dpf_unfed_20min_G008_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_unfed_20min_G009.csv -o AB_7dpf_unfed_20min_G009_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_20min_G009.csv -o AB_7dpf_unfed_20min_G009_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_20min_G009.csv -o AB_7dpf_unfed_20min_G009_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_unfed_20min_G010.csv -o AB_7dpf_unfed_20min_G010_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_20min_G010.csv -o AB_7dpf_unfed_20min_G010_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_20min_G010.csv -o AB_7dpf_unfed_20min_G010_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_unfed_20min_batch2_G001.csv -o AB_7dpf_unfed_20min_batch2_G001_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_20min_batch2_G001.csv -o AB_7dpf_unfed_20min_batch2_G001_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_20min_batch2_G001.csv -o AB_7dpf_unfed_20min_batch2_G001_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_unfed_20min_batch2_G002.csv -o AB_7dpf_unfed_20min_batch2_G002_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_unfed_20min_batch2_G002.csv -o AB_7dpf_unfed_20min_batch2_G002_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_unfed_20min_batch2_G002.csv -o AB_7dpf_unfed_20min_batch2_G002_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+
+#Fed 7dpf (6 min)
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G003.csv -o AB_7dpf_fed_new6min_G003_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_fed_new6min_G003.csv -o AB_7dpf_fed_new6min_G003_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G003.csv -o AB_7dpf_fed_new6min_G003_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G004.csv -o AB_7dpf_fed_new6min_G004_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_fed_new6min_G004.csv -o AB_7dpf_fed_new6min_G004_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G004.csv -o AB_7dpf_fed_new6min_G004_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G005.csv -o AB_7dpf_fed_new6min_G005_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_fed_new6min_G005.csv -o AB_7dpf_fed_new6min_G005_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G005.csv -o AB_7dpf_fed_new6min_G005_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G006.csv -o AB_7dpf_fed_new6min_G006_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_fed_new6min_G006.csv -o AB_7dpf_fed_new6min_G006_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G006.csv -o AB_7dpf_fed_new6min_G006_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G007.csv -o AB_7dpf_fed_new6min_G007_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_fed_new6min_G007.csv -o AB_7dpf_fed_new6min_G007_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G007.csv -o AB_7dpf_fed_new6min_G007_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G008.csv -o AB_7dpf_fed_new6min_G008_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_fed_new6min_G008.csv -o AB_7dpf_fed_new6min_G008_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G008.csv -o AB_7dpf_fed_new6min_G008_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G009.csv -o AB_7dpf_fed_new6min_G009_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_fed_new6min_G009.csv -o AB_7dpf_fed_new6min_G009_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G009.csv -o AB_7dpf_fed_new6min_G009_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G010.csv -o AB_7dpf_fed_new6min_G010_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_fed_new6min_G010.csv -o AB_7dpf_fed_new6min_G010_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G010.csv -o AB_7dpf_fed_new6min_G010_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G011.csv -o AB_7dpf_fed_new6min_G011_Foregut -c 3 1 -r 0 1A 2A 3A 4A 5A 6A 7A 20A --rdiv 20A --norm rmd,50 --evts dfy xc,3 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3_calcium.py AB_7dpf_fed_new6min_G011.csv -o AB_7dpf_fed_new6min_G011_MidHindgut -c 3 1 -r 0 8A 9A 10A 11A 12A 13A 14A 15A 16A 17A 18A 19A 20A --rdiv 20A --norm rmd,25 --evts dfy xc,3 yc,0,0 --renorm below --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
+FIBSI_v1_0_3.py AB_7dpf_fed_new6min_G011.csv -o AB_7dpf_fed_new6min_G011_Anus -c 3 1 -r 0 17A 18A 19A 20A --rdiv 20A --norm rmd,10 --evts dfy xc,1 yc,0,0 --renorm above --reevts dfy xc,3 yc,0,0 -p --plot raw fity dfy evts save_csv save_png,200
